4922aae06c5369c6a10fbef019816acc5e2fada7
Yousuf Darr <yousuf.darr@gmail.com>
2023-06-13 22:48:19 +0200

    Added alert if no username found in userPrefs, added support for master visits

--- js/extension/features/record-release-visit.js
+++ js/extension/features/record-release-visit.js
@@ -10,35 +10,41 @@
  * Overview
  * ----------------------------------------------------------
  *
- * Record release visit to database.
+ * Record release and master release visits to database.
  *
  */
 
 rl.ready(() => {
-  if (rl.pageIsNot('release')) return;
+  if (rl.pageIsNot('release') && rl.pageIsNot('master')) return;
+  // Find the type
+  const type = rl.pageIs('master') ? 'master' : 'release';
   // Get the user from preferences
   const username = rl.getPreference('username');
+  if (!username?.length) {
+    alert('Please set username in options page');
+    return;
+  }
   // Get the href and releaseId
   const href = window.location.href,
-    releaseId = href.split('/release/')[1].split('-')[0];
+    id = href?.split(`/${type}/`)[1]?.split('-')[0];
   // Define the function to record the visit
-  async function recordReleaseVisit() {
+  async function recordVisit({ type }) {
     try {
-      let url = `https://api.ozma.works/release_visit`;
+      let url = `https://api.ozma.works/${type}_visit`,
+        body = JSON.stringify({
+          username,
+          [`${type}_id`]: id,
+        });
       await fetch(url, {
         method: 'POST',
-        body: JSON.stringify({
-          username,
-          release_id: releaseId,
-        }),
+        body,
         headers: {
           'Content-Type': 'application/json',
         }
       });
     } catch (err) {
-      console.log('Error recording release visit', err);
+      console.log(`Error recording ${type} visit`, err);
     }
   }
-  // Record the visit
-  recordReleaseVisit();
+  recordVisit({ type });
 });
